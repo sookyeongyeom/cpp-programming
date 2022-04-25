0215a61b1313b824ad4a50f6fcc288d97c2e71de
sookyeongyeom <sookyeong.dev@gmail.com>
2022-04-25 15:21:21 +0900

    :books: p23

--- part_01/SimpleAdder/SimpleAdder.cpp
+++ part_01/SimpleAdder/SimpleAdder.cpp
@@ -2,15 +2,15 @@
 
 int main()
 {
-	int val1;
+	double val1;
 	std::cout << "첫 번째 숫자 입력 = ";
 	std::cin >> val1;
 
-	int val2;
+	double val2;
 	std::cout << "두 번째 숫자 입력 = ";
 	std::cin >> val2;
 
-	int result = val1 + val2;
+	double result = val1 + val2;
 	std::cout << "덧셈 결과 = " << result << std::endl;
 	return 0;
 }
---
+++ part_01/StringIO/StringIO.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+int main()
+{
+    char name[100], lang[200];
+
+    std::cout << "이름 = ";
+    std::cin >> name;
+
+    std::cout << "좋아하는 프로그래밍 언어 = ";
+    std::cin >> lang;
+
+    std::cout << "내 이름은 " << name << "입니다.\n";
+    std::cout << "제일 좋아하는 언어는 " << lang << "입니다." << std::endl;
+    return 0;
+}
