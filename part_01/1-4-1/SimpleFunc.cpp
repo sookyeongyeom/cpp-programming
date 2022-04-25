a3882bfdc96a6aeeb82e2b60974b425e0fd05d0b
sookyeongyeom <sookyeong.dev@gmail.com>
2022-04-25 18:23:20 +0900

    :books: p49

---
+++ part_01/1-4-1/SimpleFunc.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "SimpleFunc.h"
+
+void BestCom::SimpleFunc()
+{
+	std::cout << "BestCom이 정의한 함수" << std::endl;
+}
+
+void ProgCom::SimpleFunc()
+{
+	std::cout << "ProgCom이 정의한 함수" << std::endl;
+}
---
+++ part_01/1-4-1/SimpleFunc.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace BestCom
+{
+	void SimpleFunc();
+}
+
+namespace ProgCom
+{
+	void SimpleFunc();
+}
