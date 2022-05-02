77f4456e31cb0a49c0bba41d5e2916c34dc706d9
sookyeongyeom <sookyeong.dev@gmail.com>
2022-05-02 17:15:29 +0900

    :books: p410

---
+++ part_04/chapter_10/FirstOperationOverloading/FirstOperationOverloading.cpp
@@ -0,0 +1,45 @@
+﻿// FirstOperationOverloading.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class Point
+{
+    int xpos, ypos;
+public:
+    Point(int x=0, int y=0) : xpos(x), ypos(y) {}
+    void ShowPosition() const
+    {
+        cout << '[' << xpos << ", " << ypos << ']' << endl;
+    }
+    Point operator+(const Point& ref) const       // operator+라는 이름의 함수
+    {
+        Point pos(xpos + ref.xpos, ypos + ref.ypos);
+        return pos;
+    }
+};
+
+int main()
+{
+    Point pos1(3, 4);
+    Point pos2(10, 20);
+    Point pos3 = pos1.operator+(pos2);
+    Point pos4 = pos3 + pos2;
+    pos1.ShowPosition();
+    pos2.ShowPosition();
+    pos3.ShowPosition();
+    pos4.ShowPosition();
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
